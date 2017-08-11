a77cba4047fb47846d7f428f7ca8d82d33b3db76
Nadir Aboulkassimi <Nadir.Aboulkassimi@realdolmen.com>
2017-08-11 10:07:23 +0200

    jee7-jpa-02-mapping-starter gemaakt

--- jee7-jpa-02-mapping-starter/src/main/java/com/realdolmen/candyshop/domain/Candy.java
+++ jee7-jpa-02-mapping-starter/src/main/java/com/realdolmen/candyshop/domain/Candy.java
@@ -3,7 +3,41 @@ package com.realdolmen.candyshop.domain;
 import javax.persistence.*;
 
 // TODO: make candy an entity
+@Entity
 public class Candy {
 
     // TODO: add properties id (Long, pk), name (String), price (double)
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Double price;
+	@Enumerated(EnumType.STRING)
+	private CandyColor color;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public CandyColor getColor() {
+		return color;
+	}
+	public void setColor(CandyColor color) {
+		this.color = color;
+	}
+
+
 }
--- jee7-jpa-02-mapping-starter/src/main/java/com/realdolmen/candyshop/domain/Person.java
+++ jee7-jpa-02-mapping-starter/src/main/java/com/realdolmen/candyshop/domain/Person.java
@@ -3,51 +3,104 @@ package com.realdolmen.candyshop.domain;
 import com.realdolmen.candyshop.util.DateUtils;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Person {
-    @Id
-    @GeneratedValue
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    // TODO: make non nullable and restrict to 200 characters
-    private String firstName;
+	// TODO: make non nullable and restrict to 200 characters
+	@Column(nullable = false, length = 200)
+	private String firstName;
 
-    // TODO: make non nullable and restrict to 200 characters
-    private String lastName;
+	// TODO: make non nullable and restrict to 200 characters
+	@Column(nullable = false, length = 200)
+	private String lastName;
 
-    // TODO: add property birthdate (store only date portion) make it non nullable
+	// TODO: add property birthdate (store only date portion) make it non
+	// nullable
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
 
-    // TODO: add property age (not stored in database, but calculated from birthdate
+	// TODO: add property age (not stored in database, but calculated from
+	// birthdate
+	@Transient
+	private int age;
 
-    // TODO: add embedded mapping to address
+	// TODO: add embedded mapping to address
+	@Embedded
+	private Address address;
 
-    // TODO: add element collection (table name "candy_preferences", columns ("candy_color" and "person_id")
+	// TODO: add element collection (table name "candy_preferences", columns
+	// ("candy_color" and "person_id")
+	@ElementCollection
+	@CollectionTable(name = "candy_preferences")
+	@MapKeyColumn(name = "person_id")
+	@Column(name = "candy_color")
+	@Enumerated(EnumType.STRING)
+	private List<CandyColor> candyPreferences;
 
-    @PostLoad
-    public void initializeAge() {
-        this.age = DateUtils.yearsFrom(birthDate);
-    }
+	@PostLoad
+	public void initializeAge() {
+		this.age = (int) DateUtils.yearsFrom(birthDate);
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<CandyColor> getCandyPreferences() {
+		return candyPreferences;
+	}
+
+	public void setCandyPreferences(List<CandyColor> candyPreferences) {
+		this.candyPreferences = candyPreferences;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
